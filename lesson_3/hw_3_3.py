a33bec0d6f82a8fecb18ef9f9cc32fb694af466a
EugeIV <shmel54@mail.ru>
2020-12-12 06:54:07 +0700

    Урок 3 - Исправил 3_3, доделал 3_5 и 3_6

--- lesson_3/hw_3_3.py
+++ lesson_3/hw_3_3.py
@@ -1,14 +1,15 @@
 # Первый вариант
-def my_func(arg1, arg2, arg3):
+def my_func_1(arg1, arg2, arg3):
     return (arg1 + arg2 + arg3) - min(arg1, arg2, arg3)
 
 
 # Второй, но более длинный вариант
 
-# def my_func(arg1, arg2, arg3):
-#     l = [arg1, arg2, arg3]
-#     l.sort()
-#     return l[1] + l[2]
+def my_func_2(arg1, arg2, arg3):
+    l = [arg1, arg2, arg3]
+    l.sort()
+    return l[1] + l[2]
 
 
-print(my_func(12, 3, 7))
+print(my_func_1(12, 3, 7))
+print(my_func_2(12, 3, 7))
--- lesson_3/hw_3_5.py
+++ lesson_3/hw_3_5.py
@@ -1,13 +1,13 @@
-input_user = ""
+user_input = ""
+total = 0
 
-while input_user != "exit":
-    total = 0
-    input_user = input("Введите строку из чисел через пробел или exit для выхода: ")
-
-
-# l = input_user.split(" ")
-# print(l)
-# for item in l:
-#     total += int(item)
-# print(total)
+while user_input != "end":
+    user_input = input("Введите числа через пробел или 'end' для выхода: ")
+    list_input = user_input.split(" ")
+    if "end" in list_input:
+        total += sum([int(item) for item in list_input[:list_input.index("end")]], 0)
+        break
+    else:
+        total += sum([int(item) for item in list_input], 0)
+print(total)
 
